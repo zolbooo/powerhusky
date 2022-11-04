6a2a805e3de1f94482acad7ee685a322763bd12c
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-11-04 21:01:14 +0300

    feat(health): check GCP project and instance region

--- webhook/health.go
+++ webhook/health.go
@@ -12,9 +12,20 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "GITLAB_TOKEN environment variable is not defined")
 		return
 	}
+
+	if os.Getenv("GCP_PROJECT") == "" {
+		w.WriteHeader(http.StatusConflict)
+		io.WriteString(w, "GCP_PROJECT environment variable is not defined. Are you running in the GCF environment?")
+		return
+	}
 	if os.Getenv("GCE_INSTANCE_ID") == "" {
 		w.WriteHeader(http.StatusConflict)
 		io.WriteString(w, "GCP_INSTANCE_ID environment variable is not defined")
+		return
+	}
+	if os.Getenv("GCE_INSTANCE_REGION") == "" {
+		w.WriteHeader(http.StatusConflict)
+		io.WriteString(w, "GCP_INSTANCE_REGION environment variable is not defined")
 		return
 	}
 
