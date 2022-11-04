a9bd33da79e41e0cc7e43543f166e57b16f1590a
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-11-04 20:15:18 +0300

    feat(webhook): add health endpoint

---
+++ webhook/function.go
@@ -0,0 +1,29 @@
+package daemon
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("health", healthHandler)
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	if os.Getenv("GITLAB_TOKEN") == "" {
+		w.WriteHeader(http.StatusConflict)
+		io.WriteString(w, "GITLAB_TOKEN environment is not defined")
+		return
+	}
+	if os.Getenv("GCP_API_TOKEN") == "" {
+		w.WriteHeader(http.StatusConflict)
+		io.WriteString(w, "GCP_API_TOKEN environment is not defined")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
+}
