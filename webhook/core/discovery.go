1a9d5b89f1eb3cc71662ce8a36676de1008d5c1d
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-11-05 12:38:42 +0300

    feat(webhook): add GetInstanceIP implementation

---
+++ webhook/core/discovery.go
@@ -0,0 +1,33 @@
+package core
+
+import (
+	"context"
+	"os"
+
+	compute "google.golang.org/api/compute/v1"
+)
+
+func getInstance(ctx context.Context) (*compute.Instance, error) {
+	client, err := compute.NewService(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return client.Instances.Get(os.Getenv(GCP_PROJECT), os.Getenv(GCE_INSTANCE_REGION), os.Getenv(GCE_INSTANCE_ID)).Do()
+}
+
+func GetInstanceIP(ctx context.Context) (string, error) {
+	instance, err := getInstance(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	// See: https://cloud.google.com/compute/docs/instances/view-ip-address#api
+	for _, iface := range instance.NetworkInterfaces {
+		for _, accessConfig := range iface.AccessConfigs {
+			if accessConfig.NatIP != "" {
+				return accessConfig.NatIP, nil
+			}
+		}
+	}
+	return "", nil
+}
