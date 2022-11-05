e50420751695859c60790c209409cde28a7b6763
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-11-05 09:11:17 +0300

    fix(gitlab-webhook): handle incoming build events

--- webhook/handlers/gitlab.go
+++ webhook/handlers/gitlab.go
@@ -15,14 +15,45 @@ func init() {
 	hook, _ = gitlab.New(gitlab.Options.Secret(os.Getenv(core.GITLAB_TOKEN)))
 }
 
+func handleJobEvent(jobEvent gitlab.JobEventPayload) error {
+	log.Printf("Job %d is using runner %d, status is %s", jobEvent.BuildID, jobEvent.Runner.ID, jobEvent.BuildStatus)
+	return nil
+}
+func handleBuildEvent(buildEvent gitlab.BuildEventPayload) error {
+	log.Printf("Job %d is using runner %d, status is %s", buildEvent.BuildID, buildEvent.Runner.ID, buildEvent.BuildStatus)
+	return nil
+}
+
 func GitlabWebhookHandler(w http.ResponseWriter, r *http.Request) {
-	payload, err := hook.Parse(r, gitlab.JobEvents)
+	payload, err := hook.Parse(r, gitlab.JobEvents, gitlab.BuildEvents)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		log.Printf("Failed to parse request payload: %v", err)
 		return
 	}
 
-	jobEvent := payload.(gitlab.JobEventPayload)
-	log.Printf("Job %d is using runner %d, status is %s", jobEvent.BuildID, jobEvent.Runner.ID, jobEvent.BuildStatus)
+	jobEvent, ok := payload.(gitlab.JobEventPayload)
+	if ok {
+		err := handleJobEvent(jobEvent)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("failed to handle job event: %v", err)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+		return
+	}
+	buildEvent, ok := payload.(gitlab.BuildEventPayload)
+	if ok {
+		err := handleBuildEvent(buildEvent)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("failed to handle job event: %v", err)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusBadRequest)
 }
