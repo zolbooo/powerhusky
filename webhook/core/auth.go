763b9418a3d94abef397485da548575d6a70e022
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-11-05 13:25:08 +0300

    feat: token generation and verification

---
+++ webhook/core/auth.go
@@ -0,0 +1,34 @@
+package core
+
+import (
+	"crypto/hmac"
+	"crypto/sha512"
+	"crypto/subtle"
+	"encoding/base64"
+)
+
+func GenerateToken(secret string, nonce []byte) string {
+	hmac := hmac.New(sha512.New, []byte(secret))
+	hmac.Write(nonce)
+	digest := hmac.Sum(nil)
+
+	result := make([]byte, 0, len(digest)+len(nonce))
+	result = append(result, nonce...)
+	result = append(result, digest...)
+	return base64.StdEncoding.EncodeToString(result)
+}
+
+func VerifyToken(secret string, token string) bool {
+	tokenData, err := base64.StdEncoding.DecodeString(token)
+	if err != nil {
+		return false
+	}
+
+	hmac := hmac.New(sha512.New, []byte(secret))
+	nonce := tokenData[0 : len(tokenData)-hmac.Size()]
+	digest := tokenData[len(tokenData)-hmac.Size():]
+	hmac.Write(nonce)
+	computedDigest := hmac.Sum(nil)
+
+	return subtle.ConstantTimeCompare(digest, computedDigest) == 1
+}
