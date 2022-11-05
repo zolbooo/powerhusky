3a68fa250dcc9f73e391bfb05bfb32d4e1e04999
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-11-05 11:05:54 +0300

    feat(rpc): add ScheduleShutdown function

--- daemon/rpc/handler.go
+++ daemon/rpc/handler.go
@@ -1,3 +1,9 @@
 package rpc
 
+import "github.com/zolbooo/powerhusky/daemon/core"
+
 type RPCHandler struct{}
+
+func (rpc *RPCHandler) ScheduleShutdown() error {
+	return core.ScheduleShutdown()
+}
