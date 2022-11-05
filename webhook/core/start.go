f178f6258d340063113d586500bc03b0588aa486
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-11-05 12:45:00 +0300

    feat: call PushTask RPC when instance is already started

--- webhook/core/start.go
+++ webhook/core/start.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/zolbooo/powerhusky/webhook/rpc"
 	compute "google.golang.org/api/compute/v1"
 )
 
@@ -14,16 +15,36 @@ func StartInstance(ctx context.Context) error {
 		return err
 	}
 
-	op, err := client.Instances.Start(os.Getenv(GCP_PROJECT), os.Getenv(GCE_INSTANCE_REGION), os.Getenv(GCE_INSTANCE_ID)).Do()
+	instance, err := getInstance(ctx)
 	if err != nil {
 		return err
 	}
-	if op.Error != nil {
-		errData, err := op.Error.MarshalJSON()
+
+	switch instance.Status {
+	case "STOPPED":
+		op, err := client.Instances.Start(os.Getenv(GCP_PROJECT), os.Getenv(GCE_INSTANCE_REGION), os.Getenv(GCE_INSTANCE_ID)).Do()
 		if err != nil {
-			return fmt.Errorf("failed to encode operation data: %w", err)
+			return err
 		}
-		return fmt.Errorf("failed to start instance: %s", string(errData))
+		if op.Error != nil {
+			errData, err := op.Error.MarshalJSON()
+			if err != nil {
+				return fmt.Errorf("failed to encode operation data: %w", err)
+			}
+			return fmt.Errorf("failed to start instance: %s", string(errData))
+		}
+	case "RUNNING":
+		addr, err := GetInstanceIP(ctx)
+		if err != nil {
+			return err
+		}
+
+		closer, rpcClient, err := rpc.CreateClient(ctx, fmt.Sprintf("http://%s:2333", addr))
+		if err != nil {
+			return err
+		}
+		defer closer()
+		return rpcClient.PushTask(os.Getenv(DAEMON_TOKEN))
 	}
 
 	return nil
